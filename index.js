41f74272e2bb0b7605623ddb30f156a17e633d22
Touhid Hasan <touhidhasan1200@gmail.com>
2026-07-11 00:04:26 +0600

    top rated doctors add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const env = require('dotenv');
 const app = express()
 var cors = require('cors');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 env.config();
 app.use(cors());
 app.use(express.json())
@@ -19,16 +20,42 @@ const client = new MongoClient(uri, {
     }
 });
 
+const JWKS = createRemoteJWKSet(
+    new URL('http://localhost:3000/api/auth/jwks')
+)
+const verifyJWT = async (req, res, next) => {
+    const header = req.headers.authorization
+    if (!header) {
+        return res.status(401).send({ message: "Unauthorized Access" })
+    }
+    const token = header.split(' ')[1]
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorized Access" })
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, JWKS)
+        next()
+
+    } catch (error) {
+        return res.status(401).send({ message: "Forbidden Access" })
+    }
+
+}
+
 const run = async () => {
     try {
         await client.connect();
         const database = client.db('Doc-App');
         const doctorsCollection = database.collection('all-doctors');
         const bookDoctors = database.collection("appointment")
+        const usersCollection = database.collection("user")
+
+
+
         // Data Add Api 
         app.post('/all-doctor', async (req, res) => {
             const user = req.body;
-            console.log(user)
             const result = await doctorsCollection.insertOne(user);
             res.send(result);
         });
@@ -41,32 +68,53 @@ const run = async () => {
 
         // Doctors-Details Data
 
-        app.get('/all-doctors/:id', async (req, res) => {
+        app.get('/all-doctors/:id', verifyJWT, async (req, res) => {
             const { id } = req.params;
             const query = { _id: new ObjectId(id) };
             const result = await doctorsCollection.findOne(query);
             res.send(result);
-        })
+        });
 
         // Appointment Doctor Api 
-        app.post('/appointments', async (req, res) => {
+        app.post('/appointments', verifyJWT, async (req, res) => {
             const user = req.body;
             const result = await bookDoctors.insertOne(user);
             res.send(result)
-        })
+        });
 
         // Appointment api
-        app.get('/appointments/:userId', async (req, res) => {
+        app.get('/appointments/:userId', verifyJWT, async (req, res) => {
             const { userId } = req.params;
             const user = await bookDoctors.find({ userId }).toArray()
             res.send(user)
-        })
+        });
 
         // Delete Appointment
-        app.delete('/appointments/:bookId', async (req, res) => {
+        app.delete('/appointments/:bookId', verifyJWT, async (req, res) => {
             const { bookId } = req.params
             const user = await bookDoctors.deleteOne({ _id: new ObjectId(bookId) })
             res.send(user)
+        });
+
+        // Top Rated Doctors Api
+        app.get('/top-doctors', async (req, res) => {
+            const result = await doctorsCollection
+                .find()
+                .sort({ rating: -1 })
+                .limit(3)
+                .toArray();
+            res.send(result);
+        });
+        
+        // Profile Edit Api
+        app.patch('/profile/:id', async (req, res) => {
+            const { id } = req.params;
+            const updateData = req.body;
+            const updateUser = await usersCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updateData }
+            )
+            res.send(updateUser)
         })
 
 
